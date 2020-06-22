6873cc3bc32e5b8bfe2c05db66d1c9460d4c7535
Aliagho <ali.bahrami633@yahoo.com>
2020-06-22 13:48:16 +1000

    Updates

---
+++ precache-manifest.e5dfc682c7317c77af0f66204a924878.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "535a43753b97ef710d597705317b7fc4",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "235c592e2fb6f5b96405",
+    "url": "/employee-directory/static/css/main.957535ed.chunk.css"
+  },
+  {
+    "revision": "c6645cf0d3b96137ecc5",
+    "url": "/employee-directory/static/js/2.af2cef9f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/employee-directory/static/js/2.af2cef9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "235c592e2fb6f5b96405",
+    "url": "/employee-directory/static/js/main.0485ccff.chunk.js"
+  },
+  {
+    "revision": "2071748efefd41e8e77b",
+    "url": "/employee-directory/static/js/runtime-main.62fb3103.js"
+  }
+]);
